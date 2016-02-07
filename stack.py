cf0b8f444c11272425b0c5c4fe404a342c72bb90
Fran Rua <fran.rua@outlook.com>
2016-02-07 21:46:07 +0100

    Código de pila de exemplo
    Implementación de unha pila implementada en python para exempalo como exemplo
    para a charla de gpul.

---
+++ stack.py
@@ -0,0 +1,31 @@
+
+# Ejemplo de implementación de una pila
+
+
+def crear():
+    return []
+
+
+def tam(s):
+    return len(s)
+
+
+def apilar(s, e):
+    s.append(e)
+
+
+def desapilar(s):
+    t = s[-1]
+    del s[-1]
+    return t
+
+
+def cima(s):
+    return s[-1]
+
+
+def vacia(s):
+    if len(s):
+        return True
+    else:
+        return False
